ec92721609ce6dc003be1ced875a7c7e2c719502
DANGANHSON03 <anhson22062003xxx@gmail.com>
2024-10-08 18:36:57 +0700

    eson

--- src/components/Service/Service_main.js
+++ src/components/Service/Service_main.js
@@ -1,31 +1,36 @@
-import * as React from 'react';
-import Tabs from '@mui/joy/Tabs';
-import TabList from '@mui/joy/TabList';
-import Tab from '@mui/joy/Tab';
-import TabPanel from '@mui/joy/TabPanel';
-import Audi_main from './Audi/Audi_main';
-import Porsche_main from '../Porsche/Porsche';
-import Mec_main from './Mec/Mec_main';
-import Bmv_main from './BMV/Bmv_main';
-import "../../"
+import * as React from "react";
+import Tabs from "@mui/joy/Tabs";
+import TabList from "@mui/joy/TabList";
+import Tab from "@mui/joy/Tab";
+import TabPanel from "@mui/joy/TabPanel";
+import Audi_main from "./Audi/Audi_main";
+import Porsche_main from "../Porsche/Porsche";
+import Mec_main from "./Mec/Mec_main";
+import Bmv_main from "./BMV/Bmv_main";
+import "../../";
 export default function TabsBasic() {
   return (
     <Tabs aria-label="Basic tabs" defaultValue={0}>
-      <TabList sx={{height: '70px',justifyContent:'center',fontFamily: 'Roboto, sans-serif'}}>
-        <Tab sx={{width: '120px'}}>Mercedes -Benz</Tab>
-        <Tab sx={{width: '120px'}}>BMV</Tab>
-        <Tab sx={{width: '120px'}}>Audi</Tab>
-        <Tab sx={{width: '120px'}}>PORSCHE</Tab>
-
+      <TabList
+        sx={{
+          height: "70px",
+          justifyContent: "center",
+          fontFamily: "Roboto, sans-serif",
+        }}
+      >
+        <Tab sx={{ width: "120px" }}>Mercedes -Benz</Tab>
+        <Tab sx={{ width: "120px" }}>BMW</Tab>
+        <Tab sx={{ width: "120px" }}>Audi</Tab>
+        <Tab sx={{ width: "120px" }}>PORSCHE</Tab>
       </TabList>
-      <TabPanel value={0} sx={{fontFamily: 'Roboto, sans-serif'}}>
-      <Mec_main />
+      <TabPanel value={0} sx={{ fontFamily: "Roboto, sans-serif" }}>
+        <Mec_main />
       </TabPanel>
       <TabPanel value={1}>
-       <Bmv_main/>
+        <Bmv_main />
       </TabPanel>
       <TabPanel value={2}>
-       <Audi_main />
+        <Audi_main />
       </TabPanel>
       <TabPanel value={3}>
         <Porsche_main />
